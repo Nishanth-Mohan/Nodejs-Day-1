7b35be3757c9faaa1a58abe5d2c7fd9060ebeb52
Nishanth-Mohan <nishanthmohan2k15@gmail.com>
2024-05-27 09:05:45 +0530

    added endpoint

--- index.js
+++ index.js
@@ -11,8 +11,14 @@ app.use(express.json())
 
 //routes
 app.get('/',(req,res)=>{
- res.status(200).json({message:"Hi all welcome to our first node"})
+ //res.status(200).json({message:"Hi all welcome to our first node"})
+ res.status(200).send(`<span style="background-color:Aqua;color:black;font-size:100px">Welcome to our first app in node</span>`)
 })
+
+app.get("/data",(req,res)=>{
+    res.status(200).send(`<span style="background-color:Aqua;color:black;font-size:100px">Welcome to our first endpoint/data</span>`)  
+})
+
 
 app.listen(PORT,()=>{
     console.log(`App is listening on the port ${PORT}`);
